451a1cce565fcc677df677da5d00729666157d2e
Santiago Nuñez Ingas <santiago.nunez.ingas@gmail.com>
2023-07-17 20:18:36 -0300

    first commit

---
+++ src/main/java/com/santinuin/springboot/webflux/eureka/SpringBootWebfluxEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.santinuin.springboot.webflux.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringBootWebfluxEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxEurekaServerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/santinuin/springboot/webflux/eureka/SpringBootWebfluxEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santinuin.springboot.webflux.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
